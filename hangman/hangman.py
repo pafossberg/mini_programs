b9c5ff5f3a6b38acb929e344fc5948f73e4310d0
pafossberg <pietaf@protonmail.com>
2020-09-24 08:28:14 +0200

    finishing touches on hangman

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,44 +1,54 @@
 from getpass import getpass
 
-# I still need to comment out all the code, didn't have time to do this before i had to go.
-# I can probaby change alot of the "Secret_word" into "secret_phrase" function calls instead.
+def get_values() -> 'hangman()':
+    # Sets the values for 'secre_word' and 'lives' by getting input from the user.
+    # The 'secret_word value is hidden with the 'getpass' method.
 
-def get_values():
     secret_word = getpass(prompt="Please enter the 'secret' word of your choice: ").upper()
-    while secret_word.isalpha() == False:
+    # Keeps asking for input if 'secret_word' contains anything else than alphabetic characters.
+    while secret_word.isalpha() == False: 
         print("'" + secret_word + "'", "is not a legal phrase, ONLY alphabetic characters please!\n")
         secret_word = getpass(prompt="Please enter the 'secret' word of your choice: ").upper()
+
     lives = input("Please enter the amount of lives you want to add: ")
+    # Keeps asking for input if 'lives' contains anything else than digits.
     while lives.isdigit() == False:
         print("'" + lives + "'", "Is not a NUMBER.\n")
         lives = input("Please enter the amount of lives you want to add: ")
+
     hangman(secret_word, int(lives))
 
 
-def hangman(secret_word, lives):
-    secret_list = list(secret_word)
-    letter_hits = ["-" for item in secret_word]
-    used_letters = set()
+def hangman(secret_word: str, lives: int) -> 'letter_hits':
+    # Checks 'letter_hits', the amount of 'lives' and 'used_letters' and prints the results.
+    # Stops if 'letter_hits' matches 'secret_word' or if lives == 0.
 
-    while len(secret_list) > 0 and int(lives) > 0:
+    letter_hits = ["-" for item in secret_word]
+    used_letters = set() # Keeps tracks of all unique letters that have been guessed.
+
+    while letter_hits != list(secret_word) and lives > 0:
         print('\nYou have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
         print("Current word: ", " ".join(letter_hits))
-        letter = input("Guess 1 or more letters: ").upper()
-        if letter.isalpha() == False or letter in used_letters:
-            print("\n-----'" + letter + "'", "is NOT a valid character, or has already been used!-----")
-        elif letter in secret_word:
-            used_letters.add(letter)
-            for a, i in enumerate(secret_word):
-                if i in letter and i in secret_list:
-                    letter_hits[a] = i
-                    secret_list.remove(i)
+        user_guess = input("Guess 1 or more letters: ").upper()
+
+        # Checks if 'user_guess' is alphabetic and not already used.
+        if user_guess.isalpha() == True and user_guess not in used_letters:
+            used_letters.add(user_guess)
+            if user_guess in secret_word:
+
+                # Adds 'user_guess' to 'letter_hits' in the corresponding index to match 'secret_word'.
+                for index, letter in enumerate(secret_word):
+                    if letter in user_guess:
+                        letter_hits[index] = letter
+            else:
+                lives -= 1
         else:
-            lives -= 1
+            print("\n-----'" + user_guess + "'", "is NOT a valid character, or has already been used!-----")
+
     if lives == 0:
-        print('\nYou died, sorry. The word was:', "'" + secret_word + "'")
+        print('\nHangman died. The word was:', "'" + secret_word + "'")
     else:
         print('\nYAY! You guessed the word:', "'" + secret_word + "'", '!!')
 
 if __name__ == '__main__':
     get_values()
-

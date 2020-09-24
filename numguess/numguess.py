7ace332f90543dd19b68474a27a51730911af11a
pafossberg <pietaf@protonmail.com>
2020-09-24 09:25:02 +0200

    Improvements on 'numguess' script

--- numguess/numguess.py
+++ numguess/numguess.py
@@ -1,30 +1,27 @@
 from random import randint
 
-# I realized i should have swapped "lives" with "turns" and made it count turns instead of making you run out of lives.
+# Doesent check if the inputs are legal
+# Still need to comment out the code
 
-lives = 4
-print('You start off with ' + str(lives) + ' lives, for each wrong guess you lose 1 life.')
-
-num = randint(1, 10)
-
-while lives > 0:
-    best_guess = int(input('Guess a number between 1 and 10: '))
-    lives -= 1
-    if best_guess == num:
-        print('Nice guess!')
-        break
-    elif best_guess < num:
-        print('You need to guess higher')
-    elif best_guess > num:
-        print('You need to guess lower')
+def limit():
+    num_limit = input("Enter the amount of numbers, starting at 1: ")
+    return number_guess(num_limit)
 
 
-"""
+def number_guess(num_limit):
+    turns = 0
+    user_guess = int(input("\nTurn " + str(turns) + '\nGuess a number between 1 and ' + num_limit + ': '))
+    num = randint(1, int(num_limit))
 
-I Stuggled waaay to long solving this little puzzle by overthinking the "hints" the user would get, I was thinking about specific hints at first like: if your guess is not within 2 numbers of the random number you need to go up and down this and this much.
+    while user_guess != num:
+        turns += 1
+        print("\nTurn", turns)
+        if user_guess == num:
+            print('Nice guess!')
+            break
+        elif user_guess < num:
+            user_guess = int(input('You need to guess HIGHER: '))
+        elif user_guess > num:
+            user_guess = int(input('You need to guess LOWER: '))
 
-but when i realized it only had to check if the number was higher or lower and tell the user to go up or down accordingly i finished the entire program in about 10 seconds.
-
-I need to stop overthinking stuff like this!
-
-"""
+limit()

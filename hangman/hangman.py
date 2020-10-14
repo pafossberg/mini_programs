7d916802d0398711425a848d712949d29d6c9cdd
pafossberg <pietaf@protonmail.com>
2020-10-14 17:52:47 +0200

    small changes to hangman code

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,10 +1,14 @@
 from getpass import getpass
 
-def get_values() -> 'hangman()':
+
+def get_values():
     # Sets the values for 'secre_word' and 'lives' by getting input from the user.
     # The 'secret_word value is hidden with the 'getpass' method.
 
     # Checks whether the input values are valid, and keeps asking if not.
+    secret_word = getpass(
+        prompt="Please enter the 'secret' word of your choice: ").upper()
+    lives = input("Please enter the amount of lives you want to add: ")
     while True:
         try:
             if not secret_word.isalpha():
@@ -14,8 +18,9 @@ def get_values() -> 'hangman()':
                 print("\n'" + lives + "'", "Is not a NUMBER.")
                 raise Exception
             break
-        except Exception: 
-            secret_word = getpass(prompt="Please enter the 'secret' word of your choice: ").upper()
+        except Exception:
+            secret_word = getpass(
+                prompt="Please enter the 'secret' word of your choice: ").upper()
             lives = input("Please enter the amount of lives you want to add: ")
 
     hangman(secret_word, int(lives))
@@ -26,10 +31,12 @@ def hangman(secret_word: str, lives: int) -> 'letter_hits':
     # Stops if 'letter_hits' matches 'secret_word' or if lives == 0.
 
     letter_hits = ["-" for item in secret_word]
-    used_letters = set() # Keeps tracks of all unique letters that have been guessed.
+    # Keeps tracks of all unique letters that have been guessed.
+    used_letters = set()
 
     while letter_hits != list(secret_word) and lives > 0:
-        print('\nYou have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+        print('\nYou have', lives,
+              'lives left and you have used these letters: ', ' '.join(used_letters))
         print("Current word: ", " ".join(letter_hits))
         user_guess = input("Guess 1 or more letters: ").upper()
 
@@ -45,12 +52,14 @@ def hangman(secret_word: str, lives: int) -> 'letter_hits':
             else:
                 lives -= 1
         else:
-            print("\n-----'" + user_guess + "'", "is NOT a valid character, or has already been used!-----")
+            print("\n-----'" + user_guess + "'",
+                  "is NOT a valid character, or has already been used!-----")
 
     if lives == 0:
         print('\nHangman died. The word was:', "'" + secret_word + "'")
     else:
         print('\nYAY! You guessed the word:', "'" + secret_word + "'", '!!')
+
 
 if __name__ == '__main__':
     get_values()
--- hangman/hangman_improved.py
+++ hangman/hangman_improved.py
@@ -1,5 +1,6 @@
 from getpass import getpass
 
+
 class Hangman:
 
     def __init__(self, secret_word, lives):
@@ -13,6 +14,9 @@ class Hangman:
     # I.E set the "secret_word" and "lives" attributes.
     @classmethod
     def getInput(cls):
+        secret_word = getpass(
+            prompt="Please enter the 'secret' word of your choice: ")
+        lives = input("Please enter the amount of lives you want to add: ")
         while True:
             try:
                 if not secret_word.isalpha():
@@ -23,18 +27,19 @@ class Hangman:
                     raise Exception
                 break
             except Exception:
-                secret_word = getpass(prompt="Please enter the 'secret' word of your choice: ")
-                lives = input("Please enter the amount of lives you want to add: ")
+                secret_word = getpass(
+                    prompt="Please enter the 'secret' word of your choice: ")
+                lives = input(
+                    "Please enter the amount of lives you want to add: ")
         return cls(secret_word, lives)
 
-
     def matchLetter(self):
-    # Checks 'letter_hits', the amount of 'lives' and 'used_letters' and prints the results.
-    # Stops if 'letter_hits' matches 'secret_word' or if lives == 0.
+        # Checks 'letter_hits', the amount of 'lives' and 'used_letters' and prints the results.
+        # Stops if 'letter_hits' matches 'secret_word' or if lives == 0.
 
         while self.letter_hits != list(self.secret_word) and self.lives > 0:
             print('\nYou have', self.lives, 'lives left and you have used these letters: ',
-                    ' '.join(self.used_letters))
+                  ' '.join(self.used_letters))
             print("Current word: ", " ".join(self.letter_hits))
             user_guess = input("Guess 1 or more letters: ").upper()
 
@@ -50,12 +55,16 @@ class Hangman:
                 else:
                     self.lives -= 1
             else:
-                print("\n-----'" + user_guess + "'", "is NOT a valid character, or has already been used!-----")
+                print("\n-----'" + user_guess + "'",
+                      "is NOT a valid character, or has already been used!-----")
 
         if self.lives == 0:
-            print('\nHangman died. The word was:', "'" + self.secret_word + "'")
+            print('\nHangman died. The word was:',
+                  "'" + self.secret_word + "'")
         else:
-            print('\nYAY! You guessed the word:', "'" + self.secret_word + "'", '!!')
+            print('\nYAY! You guessed the word:',
+                  "'" + self.secret_word + "'", '!!')
+
 
 # un-comment line=62, and comment out line=61 to manually set attributes.
 user = Hangman.getInput()
